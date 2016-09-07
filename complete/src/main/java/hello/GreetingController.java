d9633f946dcbf948df9b125737494cb9cecca0a1
Christian Steindl <christian.steindl@allianz.de>
2016-09-07 17:24:09 +0200

    Functionality added so that rest service acceptes JSON and responds with JSON

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -4,6 +4,15 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @RestController
 public class GreetingController {
@@ -16,4 +25,18 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    @RequestMapping("/json")
+    //@Consumes(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON)
+    public JSONObject sayPlainTextHello(@RequestBody String input) {
+    	JSONObject json = new JSONObject();
+    	try {
+	    	JSONParser parser = new JSONParser();
+    		json = (JSONObject) parser.parse(input);
+    		//System.out.println("test");‚
+    	} catch (Exception e) {
+    	}
+    return json;
+  }
 }
