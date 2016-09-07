d86c252eaed82a2a09d67b214d1776cb562bccb0
Christian Steindl <christian.steindl@allianz.de>
2016-09-07 18:14:42 +0200

    Removed unused code

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,40 +1,22 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
+
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-
     @RequestMapping("/json")
-    //@Consumes(MediaType.APPLICATION_JSON)
-    //@Produces(MediaType.APPLICATION_JSON)
-    public JSONObject sayPlainTextHello(@RequestBody String input) {
+    public JSONObject readJSONandreturnJSON(@RequestBody String input) {
     	JSONObject json = new JSONObject();
     	try {
 	    	JSONParser parser = new JSONParser();
     		json = (JSONObject) parser.parse(input);
-    		//System.out.println("test");‚
     	} catch (Exception e) {
     	}
     return json;
